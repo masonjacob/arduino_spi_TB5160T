5e2b1195a07d2215be21ca6de36fa6b0b674cab2
Mason Jacob <masonjacob@uri.edu>
2024-04-12 14:49:27 -0400

    Add Trinamic Eval test code

---
+++ lib/Trinamic/TMC5130-EVAL.cpp
@@ -0,0 +1,104 @@
+#include <SPI.h>
+#include "TMC5130_registers.h"
+
+/* The trinamic TMC5130 motor controller and driver operates through an 
+ *  SPI interface.  Each datagram is sent to the device as an address byte
+ *  followed by 4 data bytes.  This is 40 bits (8 bit address and 32 bit word).
+ *  Each register is specified by a one byte (MSB) address: 0 for read, 1 for 
+ *  write.  The MSB is transmitted first on the rising edge of SCK.
+ *  
+ * Arduino Pins   Eval Board Pins
+ * 51 MOSI        32 SPI1_SDI
+ * 50 MISO        33 SPI1_SDO
+ * 52 SCK         31 SPI1_SCK
+ * 25 CS          30 SPI1_CSN
+ * 17 DIO         8 DIO0 (DRV_ENN)
+ * 11 DIO         23 CLK16
+ * GND            2 GND
+ * +5V            5 +5V
+ */
+
+int chipCS = 25;
+const byte CLOCKOUT = 11;
+int enable = 17;
+
+
+void sendData(unsigned long address, unsigned long datagram) {
+  //TMC5130 takes 40 bit data: 8 address and 32 data
+
+  delay(100);
+  unsigned long i_datagram;
+
+  digitalWrite(chipCS,LOW);
+  delayMicroseconds(10);
+
+  SPI.transfer(address);
+
+  i_datagram |= SPI.transfer((datagram >> 24) & 0xff);
+  i_datagram <<= 8;
+  i_datagram |= SPI.transfer((datagram >> 16) & 0xff);
+  i_datagram <<= 8;
+  i_datagram |= SPI.transfer((datagram >> 8) & 0xff);
+  i_datagram <<= 8;
+  i_datagram |= SPI.transfer((datagram) & 0xff);
+  digitalWrite(chipCS,HIGH);
+
+  Serial.print("Received: ");
+  Serial.println(i_datagram,HEX);
+  Serial.print(" from register: ");
+  Serial.println(address,HEX);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(chipCS,OUTPUT);
+  pinMode(CLOCKOUT,OUTPUT);
+  pinMode(enable, OUTPUT);
+  digitalWrite(chipCS,HIGH);
+  digitalWrite(enable,LOW);
+
+  //set up Timer1
+  TCCR1A = bit (COM1A0);                //toggle OC1A on Compare Match
+  TCCR1B = bit (WGM12) | bit (CS10);    //CTC, no prescaling
+  OCR1A = 0;                            //output every cycle
+
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+  SPI.setDataMode(SPI_MODE3);
+  SPI.begin();
+
+  Serial.begin(9600);
+
+  // sendData(0x80,0x00000000);      //GCONF
+
+  // sendData(0xEC,0x000101D5);      //CHOPCONF: TOFF=5, HSTRT=5, HEND=3, TBL=2, CHM=0 (spreadcycle)
+  // sendData(0x90,0x0007140A);      //IHOLD_IRUN: IHOLD=3, IRUN=10 (max.current), IHOLDDELAY=6
+  // sendData(0x91,0x0000000A);      //TPOWERDOWN=10
+
+  // sendData(0xF0,0x00000000);      // PWMCONF
+  // //sendData(0xF0,0x000401C8);      //PWM_CONF: AUTO=1, 2/1024 Fclk, Switch amp limit=200, grad=1
+
+  sendData(0xA4,0x000003E8);     //A1=1000
+  sendData(0xA5,0x000186A0);     //V1=100000
+  sendData(0xA6,0x0000C350);     //AMAX=50000
+  sendData(0xA7,0x000186A0);     //VMAX=100000
+  sendData(0xAA,0x00000578);     //D1=1400
+  sendData(0xAB,0x0000000A);     //VSTOP=10
+
+  sendData(0xA0,0x00000000);     //RAMPMODE=0
+
+  sendData(0xA1,0x00000000);     //XACTUAL=0
+  sendData(0xAD,0x00000000);     //XTARGET=0
+  sendData(0xAD,0x0007D000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // sendData(0xAD,0x0007D000);     //XTARGET=512000 | 10 revolutions with micro step = 256
+  
+  delay(1000);
+  //sendData(0x01,0x00000111);
+  // sendData(0xAD,0x00000000);     //XTARGET=0
+  // delay(20000);
+  sendData(0x01,0x00000000);
+}
--- test/main.cpp
+++ test/main.cpp
@@ -19,14 +19,14 @@
 #define ELBOW_CURRENT 1680
 
 
-// #define BASE_ROTATION_CS_PIN 10
-// #define BASE_ROTATION_STEP_PIN 9
-// #define BASE_ROTATION_CURRENT 2000
+#define BASE_ROTATION_CS_PIN 10
+#define BASE_ROTATION_STEP_PIN 9
+#define BASE_ROTATION_CURRENT 2000
 
-// #define LEFT_SHOULDER_CS_PIN 8
-// #define RIGHT_SHOULDER_CS_PIN A1
-// #define SHOULDER_STEP_PIN A0
-// #define SHOULDER_CURRENT 3000
+#define LEFT_SHOULDER_CS_PIN 8
+#define RIGHT_SHOULDER_CS_PIN A1
+#define SHOULDER_STEP_PIN A0
+#define SHOULDER_CURRENT 3000
 
 #define END_EFFECTOR_SIGNAL_PIN 4
 #define END_EFFECTOR_SWITCH_PIN 3
@@ -103,7 +103,7 @@ void driver_status(TMC5160Stepper* driver) {
     if(driver->olb())        { Serial.println(F("Open Load B"));    }
 }
 
-#define TMC5160_SPI_TEST
+#define TRINAMIC_EXAMPLE
 
 #ifdef CALIBRATE_SPREADCYCLE
 // You can define starting values here:
@@ -427,6 +427,9 @@ TMC5160_SPI motor = TMC5160_SPI(WRIST_ROTATION_CS_PIN); //Use default SPI periph
 void setup()
 {
   // USB/debug serial coms
+  pinMode(WRIST_ROTATION_CS_PIN, OUTPUT);
+  digitalWrite(WRIST_ROTATION_CS_PIN, HIGH);
+
   Serial.begin(115200);
 
 //   pinMode(SPI_DRV_ENN, OUTPUT);
@@ -490,3 +493,128 @@ void loop()
 }
 #endif
 
+#ifdef TRINAMIC_EXAMPLE
+
+
+/* The trinamic TMC5160 motor controller and driver operates through an
+* SPI interface. Each datagram is sent to the device as an address byte
+* followed by 4 data bytes. This is 40 bits (8 bit address and 32 bit word).
+* Each register is specified by a one byte (MSB) address: 0 for read, 1 for
+* write. The MSB is transmitted first on the rising edge of SCK.
+*
+* Arduino Pins Eval Board Pins
+* 51 MOSI 32 SPI1_SDI
+* 50 MISO 33 SPI1_SDO
+* 52 SCK 31 SPI1_SCK
+* 25 CS 30 SPI1_CSN
+*
+* 17 DIO 8 DIO0 (DRV_ENN)
+* 11 DIO REFL_STEP pin header
+* 4 DIO 22 SPI_MODE * 5 DIO 20 SD_MODE
+* 20 DIO DIAG0 pin header * GND 2 GND
+* +5V 5 +5V
+*/
+
+
+#define chipCS 8
+const byte STEPOUT = 11;
+int enable = 17;
+int SPImode = 4;
+int SDmode = 5;
+int DIAG0interruptpin = 20;
+unsigned long SGvalue = 0;
+unsigned long SGflag = 0;
+int cnt = 0;
+void setup() {
+//put your setup code here, to run once:
+pinMode(SPImode, OUTPUT);
+pinMode(SDmode, OUTPUT);
+pinMode(chipCS, OUTPUT);
+pinMode(STEPOUT, OUTPUT);
+pinMode(enable, OUTPUT);
+digitalWrite(SPImode, HIGH);
+digitalWrite(SDmode, HIGH);
+digitalWrite(chipCS, HIGH);
+digitalWrite(enable, HIGH);
+#ifdef diag0_interrupt
+pinMode(DIAG0interruptpin, INPUT_PULLUP);
+attachInterrupt(digitalPinToInterrupt(DIAG0interruptpin), stallguardstop, FALLING);
+#endif set up Timer1
+//for step generation
+TCCR1A = bit(COM1A0);
+//toggle OC1A on Compare Match TCCR1B = bit(WGM12) CTC, no prescaling | bit(CS10) | bit(CS12);
+//Pre scaler set to 1024 resulting in 7.8125 kHz
+OCR1A = 0;
+//output every cycle
+SPI.setBitOrder(MSBFIRST);
+SPI.setClockDivider(SPI_CLOCK_DIV8);
+SPI.setDataMode(SPI_MODE3);
+SPI.begin();
+Serial.begin(9600);
+sendData(0x80, 0x00000080);
+//GCONF -> Activate diag0_stall(Datasheet Page 31)
+sendData(0xED, 0x00000000);
+//SGT -> Needs to be adapted to get a StallGuard2 event
+sendData(0x94, 0x00000040);
+// TCOOLTHRS -> TSTEP based threshold = 55(Datasheet Page 38)
+sendData(0x89, 0x00010606);
+//SHORTCONF
+sendData(0x8A, 0x00080400);
+//DRV_CONF
+sendData(0x90, 0x00080303);
+//IHOLD_IRUN
+sendData(0x91, 0x0000000A);
+//TPOWERDOWN
+sendData(0xAB, 0x00000001);
+//VSTOP
+sendData(0xBA, 0x00000001);
+//ENC_CONST
+sendData(0xEC, 0x15410153);
+//CHOPCONF
+sendData(0xF0, 0xC40C001E);
+//PWMCONF
+digitalWrite(enable, LOW);
+}
+void loop() {
+SGvalue = sendData(0x6F, 0x00000000);
+//Read DRV_STATUS register
+SGflag = (SGvalue & 0x1000000) >> 24;
+//Check SG2 flag
+SGvalue = (SGvalue & 0x3FF);
+//Extract SG2 value bits
+Serial.print("StallGuard2 value: ");
+Serial.println(SGvalue, DEC);
+Serial.print("StallGuard2 event:");
+Serial.println(SGflag);
+}
+unsigned long sendData(unsigned long address, unsigned long datagram) {
+//TMC5130 takes 40 bit data: 8 address and 32 data
+unsigned long i_datagram = 0;
+digitalWrite(chipCS, LOW);
+delayMicroseconds(10);
+SPI.transfer(address);
+i_datagram |= SPI.transfer((datagram >> 24) & 0xff);
+i_datagram <<= 8;
+i_datagram |= SPI.transfer((datagram >> 16) & 0xff);
+i_datagram <<= 8;
+i_datagram |= SPI.transfer((datagram >> 8) & 0xff);
+i_datagram <<= 8;
+i_datagram |= SPI.transfer((datagram) & 0xff);
+digitalWrite(chipCS, HIGH);
+Serial.print("Received: ");
+Serial.println(i_datagram, HEX);
+Serial.print(" from register: ");
+Serial.println(address, HEX);
+return i_datagram;
+}
+void stallguardstop() {
+cnt++;
+// To avoid false detection.
+// When motor starts diag0 pin goes low.
+if (cnt > 1) {
+TCCR1B &= (0 << CS12);
+TCCR1B &= (0 << CS11);
+TCCR1B &= (0 << CS10);
+}
+}
+#endif
